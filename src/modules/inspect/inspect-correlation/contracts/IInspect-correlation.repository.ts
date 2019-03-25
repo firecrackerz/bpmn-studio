19ba7382da2c6ddbb7c3364f0842b9815c16b412
Alex😎 <alexander.kasten@5minds.de>
2019-03-25 11:34:32 +0100

    :sparkles: Update IInspectCorrelationRepository

--- src/modules/inspect/inspect-correlation/contracts/IInspect-correlation.repository.ts
+++ src/modules/inspect/inspect-correlation/contracts/IInspect-correlation.repository.ts
@@ -4,9 +4,8 @@ import {DataModels} from '@process-engine/management_api_contracts';
 export interface IInspectCorrelationRepository {
   getAllCorrelationsForProcessModelId(processModelId: string, identity: IIdentity): Promise<Array<DataModels.Correlations.Correlation>>;
   getLogsForCorrelation(correlation: DataModels.Correlations.Correlation, identity: IIdentity): Promise<Array<DataModels.Logging.LogEntry>>;
-  getTokenForFlowNodeInstance(
-    processModelId: string,
-    correlationId: string,
+  getTokenForFlowNodeByProcessInstanceId(
+    processInstanceId: string,
     flowNodeId: string,
     identity: IIdentity,
   ): Promise<Array<DataModels.TokenHistory.TokenHistoryEntry>>;
